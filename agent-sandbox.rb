c734667412008a30addbeadd08fd2c7437653e95
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2025-07-25 00:05:35 +0900

    feat: update agent-sandbox to v0.0.10

--- agent-sandbox.rb
+++ agent-sandbox.rb
@@ -2,10 +2,10 @@ class AgentSandbox < Formula
   desc "Run a command in a sandboxed Docker environment"
   homepage "https://github.com/iinm/dotfiles/tree/main/agent-sandbox"
 
-  url "https://raw.githubusercontent.com/iinm/dotfiles/35b377077917ec8368d256f1d595088dc1ff7c3e/agent-sandbox/bin/agent-sandbox"
-  sha256 "8fffdf1915bdc947258bb75d12bb2eefd87a0bad452cbb2fe252bbfe43bce6c4"
+  url "https://raw.githubusercontent.com/iinm/dotfiles/bf1f24b3ddec1bf134c7d87fadabf44115001aa5/agent-sandbox/bin/agent-sandbox"
+  sha256 "b84b6735d1e8ae1de5c05698cbf432da0fe159059ed64f3da7b66a55f64ee320"
 
-  version "0.0.9"
+  version "0.0.10"
 
   def install
     bin.install "agent-sandbox"
